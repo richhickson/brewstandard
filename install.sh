b421cdc6a7637aef4018f927bd755c3767ad9aa9
richhickson <rph@richardhickson.com>
2024-04-23 09:58:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/loanuser/.zprofile;
+eval "$(/opt/homebrew/bin/brew shellenv)";
 
 brew install --cask zerotier-one;
 brew install --cask firefox;
