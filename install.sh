aace0004d07f8ae9ef631da6f2f70b6132b2209e
richhickson <rph@richardhickson.com>
2024-04-23 09:57:04 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+
+brew install --cask zerotier-one;
+brew install --cask firefox;
+brew install --cask google-chrome;
+brew install --cask zoom;
+brew install --cask google-drive;
+brew install --cask microsoft-office;
